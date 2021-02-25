8cca6ab31be84535a20a1a0e4a2c939a8af54b5e
”Mihaela” <mihaela_alexandra_dinca@yahoo.com>
2021-02-25 22:40:12 +0200

    '25.02'

--- books/catalog/templatetags/check_books.py
+++ books/catalog/templatetags/check_books.py
@@ -9,7 +9,7 @@ register = template.Library()
 @register.filter(name='check_books')
 @stringfilter
 def check_books(book_instance_id, user_instance_id):
-    if Book_read.objects.filter(book_id=book_instance_id, user_id=user_instance_id).exists() is False:
+    if Book_read.objects.filter(book_id=book_instance_id, user_id=user_instance_id, active=1).exists() is False:
         return False
     else:
         return True
